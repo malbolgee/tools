a67dca73b1b89570fdddbb3e132c50bcb3945499
Malbolge <victor.gomes@icomp.ufam.edu.br>
2021-10-19 10:59:34 -0400

    feat: Add vim installation to libs script
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/libs.sh
+++ scripts/libs.sh
@@ -4,7 +4,7 @@
 install_libs() {
 
     sudo apt-get update && sudo apt-get upgrade
-    sudo apt-get -yf install python git curl build-essential apt-transport-https wine
+    sudo apt-get -yf install python git curl build-essential apt-transport-https wine vim
 }
 
 install_libs
