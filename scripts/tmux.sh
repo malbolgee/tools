8afe82185b5267a3d123a7cf62f0bb02e9765acf
Malbolge <victor.gomes@icomp.ufam.edu.br>
2021-11-15 00:01:18 -0400

    fix: Fix tmux source string
    Fix a minor problem with the tmux.conf source string.
    
    RCA: When inserting the path to the .tmux.conf from the /dotfile
    directory into the .tmux.conf file place in the ~/ directory the
    '"' symbol could cause tmux not to recognize the source.
    
    Solution: Strip the '"' symbol off.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Install tmux and puts a .tmux.conf in it
-
 install_tmux() {
 
     DOTFILES_PATH=~/dotfiles
@@ -12,15 +11,7 @@ install_tmux() {
     git clone https://malbolge.dev.br/malbolge/dotfiles.git DOTFILES_PATH
     git -C ${DOTFILES_PATH} checkout main
 
-    if [[ -f "$CONF_FILE_PATH" ]]; then
-        fl "$CONF_FILE_PATH exists."
-        echo "source-file \"${DOTFILES_PATH}/.tmux.conf\"" >> ${CONF_FILE_PATH}
-    else
-        fl "${CONF_FILE_PATH} does not exist."
-        touch "${CONF_FILE_PATH}"
-        echo "source-file \"${DOTFILES_PATH}/.tmux.conf\"" >> ${CONF_FILE_PATH}
-    fi
-
+    echo "source-file ${DOTFILES_PATH}/.tmux.conf" >> ${CONF_FILE_PATH}
 }
 
 install_tmux
