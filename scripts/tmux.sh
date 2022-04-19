3feec3060fa7ad9befd04a516baa30f6f38936e9
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-04-19 13:58:44 -0400

    refactor: Refactor the log messages in Tmux install script
    This commit refactors the log messages in the Tmux installation script.
    Now we use the new log.sh file to log messages.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+source ./log.sh
+
+LOG_TAG="Tmux Install"
+
 # Install tmux and puts a .tmux.conf in it
 install_tmux() {
 
     DOTFILES_PATH=~/dotfiles
     CONF_FILE_PATH=~/.tmux.conf
 
+    logi "${LOG_TAG}" "Trying to install Tmux.."
     sudo apt install -yf tmux
 
+    logi "${LOG_TAG}" "Setting up tmux .tmux.conf"
     git clone https://malbolge.dev.br/malbolge/dotfiles.git ${DOTFILES_PATH}
     git -C ${DOTFILES_PATH} checkout main
 
     echo "source-file ${DOTFILES_PATH}/.tmux.conf" >> ${CONF_FILE_PATH}
+
+    logi "${LOG_TAG}" "Tmux was successfully installed!"
 }
 
 install_tmux
