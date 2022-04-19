3441672427c5bafeb99cf0dc70e38852a3576821
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-04-19 14:13:39 -0400

    refactor: Refactor the log messages in Vysor install script
    This commit refactors the log messages in the Vysor installation
    script. Now we use the new log.sh file to log messages.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/vysor.sh
+++ scripts/vysor.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-# Download and install the vysor software
+source ./log.sh
 
+LOG_TAG="Vysor Install"
+
+# Download and install the vysor software
 install_vysor() {
 
-    if ! is_package_installed 'curl'; then
-        fl "Installing curl..."
-        sudo apt install -yf curl
-    fi
+	if ! is_package_installed 'curl'; then
+		logi "${LOG_TAG}" "curl is not installed, installing curl..."
+		sudo apt install -yf curl
+	fi
 
-    if ! is_package_installed 'vysor'; then
-        fl "Downloading Vysor..."
+	if ! is_package_installed 'vysor'; then
+		logi "${LOG_TAG}" "Downloading Vysor.."
 
-	wget --no-check-certificate\
-	--content-disposition\
-	--show-progress\
-	https://github.com/malbolgee/tools/releases/download/v0.1/vysor.AppImage
+		wget --no-check-certificate\
+		--content-disposition\
+		--show-progress\
+		https://github.com/malbolgee/tools/releases/download/v0.1/vysor.AppImage
 
-    sudo chmod +x vysor.AppImage
-    sudo mv vysor.AppImage /usr/bin
-    sudo ln -s /usr/bin/vysor.AppImage /usr/bin/vysor
-    else
-        fl "Vysor is already installed."
-    fi
+		logi "${LOG_TAG}" "Changing the vysor.AppImage execution status"
+		sudo chmod +x vysor.AppImage
+		logi "${LOG_TAG}" "Moving vysor.AppImage to /usr/bin"
+		sudo mv vysor.AppImage /usr/bin
+		logi "${LOG_TAG}" "Creating Symbolic Link vysor.AppImage -> vysor"
+		sudo ln -s /usr/bin/vysor.AppImage /usr/bin/vysor
+	else
+		logi "${LOG_TAG}" "Vysor is already installed."
+	fi
+
+    logi "${LOG_TAG}" "Vysor was successfully installed!"
 }
 
 install_vysor
