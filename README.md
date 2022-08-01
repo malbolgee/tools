89715e46713bc47d693f4067efb6af70fd4c26c7
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-07-31 22:30:01 -0400

    feat: Implement new gitconfig configure method and upadate README file
    This commit implement the a new method to configure the .gitconfig file
    in the machine. This commit also updates the README file with more
    information about the behavior of some scripts. This commit also
    refactor the creation of the server config file.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 ## Tools
 
----
-
 This repository is a set of scripts aiming to help to easily create the development workspace needed for the team.
 
-In the scripts/ directory you will find several scripts in which the entry point is the ```config.sh``` file.
+In the scripts/ directory you will find several scripts in which the entry point is the ```main.sh``` file.
 
 Maybe you'll need to make this script executable:
 
 ```shell
-$ chmod +x config.sh
+$ chmod +x main.sh
 ```
 
 After that:
 
 ```shell
-$ ./config.sh
+$ ./main.sh
 ```
 
 This script has several flags of options:
@@ -30,13 +28,36 @@ This script has several flags of options:
 > **-s, --ssh** - Create the ssh key and set the config file.<br>
 > **-h, --help** - Show how to use the program.<br>
 
-*For now, this flags cannot be used concomitantly.*
-
 So, this is a valid use of the script:
 
 ```shell
-$ ./config -a
+$ ./main -a
 ```
 
 This command will install everything. Be aware that sometimes you'll have to give yes or no answers to the prompt, so you can't just run the script and go lunch.
+
+## The Android Studio Installation
+
+The Android Studio installation can be triggered by choosing the ```-a | --all``` flags, or by specifically selecting to install only this program with the ```-A | --android``` flag. The script will download the most up-to-date Android Studio binary from its repository and, at some point, will question you if you want to install it. It is recommended to say yes to this question because after the installation, we export the platform-tools directory to ```$PATH```, so you can access tools such as ```adb``` and ```fastboot```.
+
+## The SSH configuration
+
+This script will set a SSH key into your machine. This is necessary to access tools such as ```Gerrit``` and to make it easier to login into the *build server*. Follow [this tutorial](https://docs.google.com/document/d/1UFVoLMMWVDtZdRW41DAtouhqyThGHwrxl9KKC4NWihY/edit#) to configure Gerrit with your SSH key.
+
+After this script run, you'll be able to login into the build server just by typing:
+
+```shell
+$ ssh zbr|indt # zbr OR indt
+```
+
+```zbr``` to login into the Jaguariúna build server
+```indt``` to login into the INDT build server
+
+### Set your SSH key into the build server
+
+To set your SSH key into the build server and free yourself of the need to type your user and password every time, simply:
+
+```shell
+$ ssh-copy-id -i <path/to/your/key/id_coreid>.pub <zbr|indt>
+```
 
--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -7,63 +7,63 @@ LOG_TAG="SSH Config"
 COREID=""
 
 config_ssh() {
-    prompt_coreid_question
-    generate_key
-    add_key_to_authorized_keys
-    download_template_config_file
-    fill_template_file
-    move_template_file
-    change_permissions
+	prompt_coreid_question
+	generate_key
+	add_key_to_authorized_keys
+
+	# only configure server file if we are not on a sever itself
+	[ "zbr05lbld12" != "$(uname -n)" ] && configure_config_file
+
+	change_permissions
+	configure_gitconfig
 }
 
 change_permissions() {
-    logi "${LOG_TAG}" "Changing directories permissions.."
-    sudo chmod 755 ~
-    sudo chmod -R 700 ~/.ssh
+	logi "${LOG_TAG}" "Changing directories permissions.."
+	sudo chmod 755 ~
+	sudo chmod -R 700 ~/.ssh
 }
 
 add_key_to_authorized_keys() {
-    logi "${LOG_TAG}" "Adding key to authorized keys.."
-    cat id_"$COREID".pub >> authorized_keys
-}
-
-fill_template_file() {
-    sed -i "s/coreid/${COREID}/g" ./config
-}
-
-move_template_file() {
-    logi "${LOG_TAG}" "Moved config file to ~/.ssh/"
-    mv ./config ~/.ssh/
+	logi "${LOG_TAG}" "Adding key to authorized keys.."
+	cat id_"$COREID".pub >> authorized_keys
 }
 
 prompt_coreid_question() {
-    read -p "Is \"${USER}\" your coreid? [yN] " yn
+	read -p "Is \"${USER}\" your coreid? [yN] " yn
 
-    if [[ "$yn" =~ [yY] ]] || [ -z "$yn" ]; then
-        COREID="$USER"
-    else
-        while true; do
-            read -p "Provide your coreid: " coreid
-        
-            if [ -n "$coreid" ]; then
-                COREID="$coreid"
-                break
-            fi
-        done
-    fi
+	if [[ "$yn" =~ [yY] ]] || [ -z "$yn" ]; then
+		COREID="$USER"
+	else
+		while true; do
+			read -p "Provide your coreid: " coreid
+
+			if [ -n "$coreid" ]; then
+				COREID="$coreid"
+				break
+			fi
+		done
+	fi
 }
 
 generate_key() {
-    logi "${LOG_TAG}" "Generating SSH key.."
-    yes '' | ssh-keygen -t rsa -f ~/.ssh/id_"$COREID"
+	logi "${LOG_TAG}" "Generating SSH key.."
+	yes '' | ssh-keygen -t rsa -f ~/.ssh/id_"$COREID"
 }
 
-download_template_config_file() {
-    logi "${LOG_TAG}" "Downloading config file..."
-    wget --no-check-certificate\
-    --content-disposition\
-    --show-progress\
-	https://github.com/malbolgee/tools/releases/download/v0.1/config
+configure_gitconfig() {
+	logi "${LOG_TAG}" "Configuring your .gitconfig file"
+
+	# TODO: handle if we are in SSH access in ZBR or INDT
+
+	cp "$(dirname "$(pwd)")"/.assets/.gitconfig "${HOME}"/.gitconfig
+	sed -i "s/coreid/${COREID}/g" "${HOME}"/.gitconfig
+}
+
+configure_config_file() {
+	logi "${LOG_TAG}" "Configuring server config file"
+	cp "$(dirname "$(pwd)")"/.assets/config "${HOME}"/.ssh/config
+	sed -i "s/coreid/${COREID}/g" "${HOME}"/.ssh/config
 }
 
 config_ssh
