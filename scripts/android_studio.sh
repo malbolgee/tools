b4015f980523103888ad3be5029a9df193b41790
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-08-04 11:42:27 -0400

    refactor: Refactor Android Studio installation script
    This commit refactors the Android Studio Installation function. We
    remove the necessity of downloading the rules files. Now, this file
    comes with the repository and it is simply copied to its destination.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/android_studio.sh
+++ scripts/android_studio.sh
@@ -13,70 +13,75 @@ LOG_TAG="Android Studio Install"
 # {@link http://www.linux-usb.org/usb.ids}
 #
 install_android_studio() {
+	logi "${LOG_TAG}" "Adding maarten-fonville/android-studio repository"
+	sudo add-apt-repository -y 'ppa:maarten-fonville/android-studio'
+	sudo apt-get update
 
-    sudo add-apt-repository -y 'ppa:maarten-fonville/android-studio'
-    sudo apt-get update
-    logi "${LOG_TAG}" "Installing Android Studio"
-    sudo apt-get install -yf android-studio
+	logi "${LOG_TAG}" "Installing Android Studio"
+	sudo apt-get install -yf android-studio
 
-    cr=`echo $'\n.'`
-    cr=${cr%.}
-    prompt="Are you sure? 'platform-tools' directory won't be put into PATH if you proceed.${cr}"
-    prompt="${prompt}This means that you won't be able to use tools like adb or fastboot right off the bat. [Y/n] "
+	cr=`echo $'\n.'`
+	cr=${cr%.}
+	prompt="Are you sure? 'platform-tools' directory won't be put into PATH if you proceed.${cr}"
+	prompt="${prompt}This means that you won't be able to use tools like adb or fastboot right off the bat. [Y/n] "
 
-    while true; do
-        read -p "Do you want to setup Android Studio now? [Y/n] " yn
-        case $yn in
-            [Yy]* )
-                lunch_android_studio_and_export
-                break
-                ;;
-            [Nn]* )
-                while true; do
-                    read -p "${prompt}" yn
-                    case $yn in
-                        [Yy]* )
-                            logw "${LOG_TAG}" "Continuing without the export."
-                            break
-                            ;;
-                        [Nn]* )
-                            lunch_android_studio_and_export
-                            break
-                            ;;
-                        * )
-                            loge "${LOG_TAG}" "Please answer yes or no."
-                            ;;
-                    esac
-                done
-                break
-                ;;
-            * )
-                loge "${LOG_TAG}" "Please answer yes or no."
-                ;;
-        esac
-    done
+	while true; do
+		read -p "Do you want to setup Android Studio now? [Y/n] " yn
+		case $yn in
+			[Yy]* )
+				lunch_android_studio_and_export
+				break
+				;;
+			[Nn]* )
+				while true; do
+					read -p "${prompt}" yn
+					case $yn in
+						[Yy]* )
+							logw "${LOG_TAG}" "Continuing without the export."
+							break
+							;;
+						[Nn]* )
+							lunch_android_studio_and_export
+							break
+							;;
+						* )
+							loge "${LOG_TAG}" "Please answer yes or no."
+							;;
+					esac
+				done
+				break
+				;;
+			* )
+				loge "${LOG_TAG}" "Please answer yes or no."
+				;;
+		esac
+	done
 
-    if ! is_package_installed 'curl'; then
-        logi "${LOG_TAG}" "curl is not installed, installing curl..."
-        sudo apt install -yf curl
-    fi
+	logi "${LOG_TAG}" "Configuring rules file"
+	if [ ! -d "/etc/udev/rules.d" ]; then
+		logi "${LOG_TAG}" "/etc/udev/rules.d/ does not exist, creating it"
+		sudo mkdir -p /etc/udev/rules.d/
+	fi
 
-    sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
-    sudo chmod a+r /etc/udev/rules.d/51-android.rules
-    sudo service udev restart
+	logi "${LOG_TAG}" "Copying rules file to final destination"
+	sudo cp "$(dirname "$(pwd)")"/.assets/51-android.rules /etc/udev/rules.d/51-android.rules
+	sudo chmod a+r /etc/udev/rules.d/51-android.rules
+	sudo service udev restart
 
-    # The fwupd linux service somehow interferes with the fastboot process. We
-    # need to disable it.
-    stop_service fwupd
+	# The fwupd linux service somehow interferes with the fastboot process. We
+	# need to disable it.
+	stop_service fwupd
+
+	logi "${LOG_TAG}" "Android Studio Setup is done."
 }
 
 lunch_android_studio_and_export() {
-    logi "${LOG_TAG}" "Launching Android Studio Setup Wizard"
-    /opt/android-studio/bin/./studio.sh
+	logi "${LOG_TAG}" "Launching Android Studio Setup Wizard"
+	/opt/android-studio/bin/./studio.sh
 
-    # here we are counting on that the user has completed the
-    # setup wizard, otherwise, the export will fail.
-    path_export "$HOME/Android/Sdk/platform-tools"
+	# here we are counting on that the user has completed the
+	# setup wizard, otherwise, the export will fail.
+	path_export "$HOME/Android/Sdk/platform-tools"
 }
 
 install_android_studio
