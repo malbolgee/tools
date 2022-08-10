f5f5f8c8d27dbfae9080924d3aaaeae1f1190aa8
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-08-09 20:40:54 -0400

    feat: Add additional essential programs to install
    Add sqlitebrowser
    Add audit2allow
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/libs.sh
+++ scripts/libs.sh
@@ -3,9 +3,10 @@
 # Install all the libs necessary for the other packages to properly run.
 install_libs() {
 
-    sudo apt update && sudo apt-get upgrade
-    sudo apt -yf install python git curl build-essential apt-transport-https vim
-    sudo apt -yf install default-jre default-jdk
+	sudo apt update && sudo apt-get upgrade
+	sudo apt -yf install python git curl build-essential apt-transport-https vim
+	sudo apt -yf install default-jre default-jdk sqlitebrowser
+	sudo apt -yf install sqlitebrowser policycoreutils-python-utils
 }
 
 install_libs
