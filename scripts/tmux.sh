9681432fb114f8ad259d9126ad22231cf7ab0b30
Malbolge <victor.gomes@icomp.ufam.edu.br>
2024-05-16 08:32:43 -0400

    feat(#4): Check if tmux is installed before trying to install it
    Close #4
    
    This commit adds a verification to whether tmux is installed or not.
    This is necessary because this script can run in the build server where
    the user will, most likely, not have sudo privileges.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -7,16 +7,18 @@ LOG_TAG="Tmux Install"
 # Install tmux and puts a .tmux.conf in it
 install_tmux() {
 
-    DOTFILES_PATH=~/dotfiles
-    CONF_FILE_PATH=~/.tmux.conf
+    DOTFILES_PATH=$HOME/dotfiles
+    CONF_FILE_PATH=$HOME/.tmux.conf
 
-    logi "${LOG_TAG}" "Trying to install Tmux.."
-    sudo apt install -yf tmux
+    if ! is_package_installed 'tmux'; then
+        logi "${LOG_TAG}" "Trying to install Tmux.."
+        sudo apt install -yf tmux
+    fi
 
     logi "${LOG_TAG}" "Setting up tmux .tmux.conf"
-    git clone https://github.com/malbolgee/dotfiles.git ${DOTFILES_PATH}
+    git clone https://github.com/malbolgee/dotfiles.git "${DOTFILES_PATH}"
 
-    echo "source-file ${DOTFILES_PATH}/.tmux.conf" >> ${CONF_FILE_PATH}
+    echo "source-file ${DOTFILES_PATH}/.tmux.conf" >> "${CONF_FILE_PATH}"
 
     logi "${LOG_TAG}" "Tmux was successfully installed!"
 }
